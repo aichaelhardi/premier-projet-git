c6438fea622d651a29695933afdad2e7bce8764a
aicha <aicha@gmail.com>
2026-09-21 16:41:58 +0000

    challenges h2.0

--- boucle.js
+++ boucle.js
@@ -1,8 +1,4 @@
-const prompt = require(`prompt-sync`)();
-let nombre =[1,2,3,4,5];
-for (let i=1 ;i<nombre.length;i++){
-   if (i === 3){
-   continue;
-   }
-   console.log(`${i}`);
-}
+const prompt = require('prompt-sync')();
+let nombre = Number(prompt("donner un nombre"));
+for (let i=1 ; i <=3 ;i++)
+    console.log(i)
